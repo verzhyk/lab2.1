b5bfd1a31a12335d63274ad19ffae074d492d13c
Влад Верожинский <vladyslav.verozhynskyi.itis.2020@lpnu.ua>
2021-03-23 15:22:49 +0200

    create project

---
+++ labaVlad/FloatPower.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labaVlad
+{
+    public class FloatPower
+    {
+        private float first;
+
+        public float First
+        {
+            get { return first; }
+            set { first = value; }
+        }
+
+        private float second;
+
+        public float Second
+        {
+            get { return second; }
+            set { second = value; }
+        }
+
+
+        public FloatPower(float first, float second)
+        {
+            First = first;
+            Second = second;
+        }
+
+        public double Power() => Math.Pow(First, Second);
+
+        public static FloatPower operator ++(FloatPower floatPower)
+        {
+            return new FloatPower(floatPower.first, floatPower.second + 1);
+        }
+
+        public static FloatPower operator --(FloatPower floatPower)
+        {
+            return new FloatPower(floatPower.first, floatPower.second - 1);
+        }
+
+    }
+}
---
+++ labaVlad/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labaVlad
+{
+    class Program
+    {
+
+        public static FloatPower Init(float first, float second)
+        {
+            if (first > 0 && second > 0) return new FloatPower(first, second);
+            else return null;
+        }
+
+        public static void Read()
+        {
+            Console.Write("Enter first value: ");
+            float.TryParse(Console.ReadLine(), out float first);
+            Console.Write("Enter second value: ");
+            float.TryParse(Console.ReadLine(), out float second);
+
+            Display(Init(first, second));
+        }
+
+        public static void Display(FloatPower floatPower)
+        {
+            if (floatPower != null)
+            {
+                Console.WriteLine(floatPower.Power());
+                floatPower--;
+                Console.WriteLine(floatPower.Power());
+                floatPower++;
+                Console.WriteLine(floatPower.Power());
+            }
+            else
+            {
+                Console.WriteLine("Введены неверные значения!");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Read();
+        }
+    }
+}
